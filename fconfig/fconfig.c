5cbf8b5450fd8aba986c50cfe0fd47adbc787309
Rich Stephens <richswww@gmail.com>
2026-03-07 11:20:35 -0500

    Cleanup, added fujinet version (#35)

--- fconfig/fconfig.c
+++ fconfig/fconfig.c
@@ -41,7 +41,7 @@ void fetch_adapter_config(void)
 
     if (r.h.al != 'C')
     {
-        printf("ERROR: Could not read adapter configuration.\n");
+        printf("ERROR:  Could not read adapter configuration.\n");
         exit(1);
     }
 }
@@ -49,14 +49,15 @@ void fetch_adapter_config(void)
 void show_adapter_config(void)
 {
     printf("\nCurrent FUJINET Network Config\n\n");
-    printf("%8s:%30s\n","SSID",ac.ssid);
-    printf("%8s:%30s\n","Hostname",ac.hostname);
-    printf("%8s:%3u.%3u.%3u.%3u\n","Local IP",ac.localIP[0],ac.localIP[1],ac.localIP[2],ac.localIP[3]);
-    printf("%8s:%3u.%3u.%3u.%3u\n","Netmask",ac.netmask[0],ac.netmask[1],ac.netmask[2],ac.netmask[3]);
-    printf("%8s:%3u.%3u.%3u.%3u\n","Gateway",ac.gateway[0],ac.gateway[1],ac.gateway[2],ac.gateway[3]);
-    printf("%8s:%3u.%3u.%3u.%3u\n","DNS",ac.dnsIP[0],ac.dnsIP[1],ac.dnsIP[2],ac.dnsIP[3]);
-    printf("%8s:%02X:%02X:%02X:%02X:%02X:%02X\n","MAC",ac.macAddress[0],ac.macAddress[1],ac.macAddress[2],ac.macAddress[3],ac.macAddress[4],ac.macAddress[5]);
-    printf("%8s:%02X:%02X:%02X:%02X:%02X:%02X\n","BSSID",ac.bssid[0],ac.bssid[1],ac.bssid[2],ac.bssid[3],ac.bssid[4],ac.bssid[5]);    
+    printf("%8s: %-30s\n","SSID",ac.ssid);
+    printf("%8s: %-30s\n","Hostname",ac.hostname);
+    printf("%8s: %u.%u.%u.%u\n","Local IP",ac.localIP[0],ac.localIP[1],ac.localIP[2],ac.localIP[3]);
+    printf("%8s: %u.%u.%u.%u\n","Netmask",ac.netmask[0],ac.netmask[1],ac.netmask[2],ac.netmask[3]);
+    printf("%8s: %u.%u.%u.%u\n","Gateway",ac.gateway[0],ac.gateway[1],ac.gateway[2],ac.gateway[3]);
+    printf("%8s: %u.%u.%u.%u\n","DNS",ac.dnsIP[0],ac.dnsIP[1],ac.dnsIP[2],ac.dnsIP[3]);
+    printf("%8s: %02X: %02X: %02X: %02X: %02X: %02X\n","MAC",ac.macAddress[0],ac.macAddress[1],ac.macAddress[2],ac.macAddress[3],ac.macAddress[4],ac.macAddress[5]);
+    printf("%8s: %02X: %02X: %02X: %02X: %02X: %02X\n","BSSID",ac.bssid[0],ac.bssid[1],ac.bssid[2],ac.bssid[3],ac.bssid[4],ac.bssid[5]);    
+    printf("%8s: %-30s\n","FNVER",strupr(ac.fn_version));
 }
 
 void main(void)
